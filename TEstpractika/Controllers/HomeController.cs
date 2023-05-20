dbde3e3a74d3eb86835c695ca28350eecaaddb13
sahinmelikov <1rima621melikov@gmail.com>
2023-05-20 09:53:37 +0400

    ss

---
+++ TEstpractika/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TEstpractika.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
